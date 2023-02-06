3e494aa9bcb3dac2f4705e1c05d4d5fb8c64c5a4
seri lee <serirucy@gmail.com>
2023-02-07 06:23:13 +0900

    maplevel

--- js/js_sub1.js
+++ js/js_sub1.js
@@ -2,7 +2,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
   mapOption = {
     center: new kakao.maps.LatLng(37.298318, 127.0090137), // 지도의 중심좌표
-    level: 16 // 지도의 확대 레벨
+    level: 3 // 지도의 확대 레벨
   };
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 // 마커가 표시될 위치입니다 
